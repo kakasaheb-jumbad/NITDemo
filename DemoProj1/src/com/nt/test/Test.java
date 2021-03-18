3a182b6356d572b9d15e1f82f3bfac3859670832
dev2 <dev2@gmail.com>
2021-03-18 18:47:12 +0530

    Dev1 Modified test.java

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -1,5 +1,8 @@
 package com.nt.test;
-
+import com.nt.beans.*;
 public class Test {
-
+	public static void main(String[] args) {
+		Utility u= new Utility();
+		System.out.println(u.getSysDate());
+	}
 }
